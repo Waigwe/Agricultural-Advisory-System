3eb03c4e6dc72ab153ff51892fb882b8c23b4651
Catherine Waigwe <kwaigwe@gmail.com>
2020-08-18 16:14:28 +0300

    Initial commit

---
+++ lib/src/main/models/Sensor.dart
@@ -0,0 +1,17 @@
+class Sensor {
+  final String soilMoisture;
+  final String pH;
+  final String farmerName;
+  final String farmerUid;
+  final String time;
+
+
+  Sensor({
+    this.soilMoisture,
+    this.pH,
+    this.farmerName,
+    this.farmerUid,
+    this.time,
+ 
+  });
+}
